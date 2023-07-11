13aa0790b057109368ded814b6c725195a60998f
Aneel Grewal <aneel.grewal@hotmail.com>
2023-07-12 00:08:51 +0200

    showOrderSummary & updateWallet promises created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cart = ["shoes", "shirts", "pants"];
 
 createOrder(cart)
   .then(function (orderId) {
-    console.log("order ID : ", orderId);
+    console.log("Order ID : ", orderId);
     return orderId;
   })
   .then(function (orderId) {
@@ -10,6 +10,20 @@ createOrder(cart)
   })
   .then(function (paymentInfo) {
     console.log("Payment : ", paymentInfo);
+    return paymentInfo;
+  })
+  .then(function (paymentInfo) {
+    return showOrderSummary(paymentInfo);
+  })
+  .then(function (summary) {
+    console.log("Order summary : ", summary.join(", "));
+    return summary;
+  })
+  .then(function (summary) {
+    return updateWallet(summary);
+  })
+  .then(function (wallet) {
+    console.log(wallet);
   })
   .catch(function (err) {
     console.log(err.message);
@@ -31,7 +45,7 @@ function createOrder(cart) {
 }
 
 function validateCart() {
-  return false;
+  return true;
 }
 
 function proceedToPayment(orderId) {
@@ -48,4 +62,29 @@ function proceedToPayment(orderId) {
 
 function paymentVerified() {
   return true;
+}
+
+function showOrderSummary(paymentInfo) {
+  return new Promise(function (resolve, reject) {
+    if (!paymentInfo) {
+      const err = new Error("error in orderSummary");
+      reject(err);
+    }
+    resolve(cart);
+  });
+}
+
+function updateWallet(summary) {
+  return new Promise(function (resolve, reject) {
+    if (!wallet()) {
+      const err = new Error("Error in wallet");
+      reject(err);
+    }
+
+    resolve("Wallet updated!");
+  });
+}
+
+function wallet() {
+  return false;
 }
